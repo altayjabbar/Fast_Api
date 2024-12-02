f206c0d2d8c01dcc0054a423b2cf2442c306467b
altayjabbar <jabbarofaltay@gmail.com>
2024-12-02 16:56:07 +0400

    enum

--- fastapi_tutorial/main.py
+++ fastapi_tutorial/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI # type: ignore
+from enum import Enum
+from fastapi import FastAPI  # type: ignore
 
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
@@ -9,13 +11,37 @@ async def root():
 
 @app.post("/")
 async def post():
-    return {"message":"hello by post owner"}
+    return {"message": "hello by post owner"}
 
 
 @app.put("/")
 async def post():
-    return {"message":"put fist post"}
+    return {"message": "put fist post"}
+
 
 @app.delete("/")
 async def delete():
-    return {"message":"delete all post"}
+    return {"message": "delete all post"}
+
+
+class FoodEnum(str, Enum):
+    fruits = "fruits"
+    vegetables = "vegetables"
+    dairy = "dairy"
+
+
+@app.get("/foods/{food_name}")
+async def get_food(food_name: FoodEnum):
+
+    if food_name == FoodEnum.fruits:
+        return {"food_name": food_name, "message": "you are healthy"}
+
+    if food_name.value == "fruits":
+        return {"food_name": food_name, "message": "this fantastic"}
+
+    if food_name == FoodEnum.vegetables:
+        return {"food_name": food_name, "message": "this is natural"}
+
+    if food_name == FoodEnum.dairy:
+        return {"food_name": food_name, "message": "this is natural"}
+    return {"food_name": "food_name", "message": "this is perfect competetion"}
